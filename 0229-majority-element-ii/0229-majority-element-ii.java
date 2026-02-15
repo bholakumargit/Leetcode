33e976928acd3cdbb04dbf9150e3d5ffdbc25705
bholakumargit <vikashbhola9089@gmail.com>
2026-02-15 11:36:53 +0530

    Time: 2 ms (99.64%), Space: 52.7 MB (50.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,22 +3,22 @@ class Solution {
         int c1 = 0, c2 = 0;
         int n1 = 0, n2 = 0;
 
-        for (int n : nums) {
+        for(int n : nums) {
 
             if (n == n1) c1++;
              else if (n == n2) c2++;
 
-            else if (c1 == 0) {
+            else if(c1 == 0) {
                 n1 = n;
                 c1 = 1;
             }
 
-            else if (c2 == 0) {
+            else if(c2 == 0) {
                 n2 = n;
                 c2 = 1;
             }
 
-            else {
+            else{
                 c1--;
                 c2--;
             }
